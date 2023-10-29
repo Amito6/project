8b1da8ad6701dbf6ee144c8c0d5f45493ae6e2d8
amito6 <amitchain24@gmail.com>
2023-10-28 20:27:15 -0400

    36-13

--- src/Component/Signup/Signup.jsx
+++ src/Component/Signup/Signup.jsx
@@ -36,12 +36,64 @@ const Signup =() =>{
 
     const requiredValidation = (e)=>{
         const input = e.target;
-        required(input);
+        const key = input.name
+        const isRequired =  required(input);
+        setError((oldData)=>{
+        return{
+            ...oldData,
+            [key]:isRequired
+        }
+
+       })
+    }
+    const emailValidation = (e)=>{
+        const input = e.target;
+        const key = input.name
+        const isRequired =  required(input);
+        const isEmail = emailSyntax(input);
+        console.log(isEmail);
+        setError((oldData)=>{
+        return{
+            ...oldData,
+            [key]: (isRequired.state && isRequired) || isEmail
+        }
+
+       })
     }
 
     const required = (input)=>{
-        alert();
-    }
+        let value = input.value; 
+        if(value.length === 0){
+            return{
+                state:true,
+                message: "This Field is required"
+            }
+        }
+        else{
+            return{
+                state:false,
+                message: ""
+            }
+        }
+        }
+
+    const emailSyntax = (input) =>{
+        const value= input.value.trim();
+        const regexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g
+        if(regexp.test(value)){
+           return{
+            state: false,
+            message: ""
+           }
+        }
+        else{
+            return {
+                state:true,
+                message:"This is not valid email"
+            }
+        }
+    }    
+    
     
     const updateValue = (e)=>{
         const input = e.target;
@@ -58,7 +110,7 @@ const Signup =() =>{
     const design = (
         <>
         <Grid container>
-            <Grid item sm="7" xs="12">
+            <Grid item sm={7} xs={12}>
                 <img src="images/auth-big.avif" width="100%" alt="" />
             </Grid>
             <Grid item sx={{
@@ -66,7 +118,7 @@ const Signup =() =>{
                     xs:3,
                     sm:7
                 }
-            }} sm="5" xs="12" >
+            }} sm={5} xs={12} >
                 <Typography sx={{
                     mt:{
                         xs:0,
@@ -81,10 +133,10 @@ const Signup =() =>{
                 }
                 <form>
                     <Stack spacing={3} >
-                        <TextField error={error.fullName.state} helperText={error.fullName.message} label="FullName" name="fullName" value={input.fullName} onChange={updateValue} onBlur={requiredValidation} />
-                        <TextField error={error.mobile.state} label="Mobile" type="number" name="mobile" value={input.mobile} onChange={updateValue} />
-                        <TextField error={error.email.state} label="Email" name="email" value={input.email} onChange={updateValue} />
-                        <TextField error={error.password.state} label="Password" type="Password" name="password" value={input.password} onChange={updateValue} />
+                        <TextField error={error.fullName.state} helperText={error.fullName.message} label="FullName" name="fullName" value={input.fullName} onChange={updateValue} onBlur={requiredValidation} onInput={requiredValidation} />
+                        <TextField error={error.mobile.state} helperText={error.mobile.message} label="Mobile" type="number" name="mobile" value={input.mobile} onChange={updateValue} onBlur={requiredValidation} onInput={requiredValidation}  />
+                        <TextField error={error.email.state} helperText={error.email.message} label="Email" name="email" value={input.email} onChange={updateValue} onBlur={emailValidation} onInput={emailValidation}  />
+                        <TextField error={error.password.state} helperText={error.password.message} label="Password" type="Password" name="password" value={input.password} onChange={updateValue} onBlur={requiredValidation} onInput={requiredValidation}  />
                         <Stack direction={"row"}
                         justifyContent="space-between"
                         alignItems="center">
